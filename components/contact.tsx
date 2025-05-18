f92c4d97e8f4f6c7206b431f4722b3dea0ddf192
Nikitahh <nikitahsieh03@gmail.com>
2025-05-18 23:43:12 +0800

    contact section

---
+++ components/contact.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+import { FaPaperPlane } from 'react-icons/fa'
+
+export default function Contact() {
+	return (
+		<section id="contact" className="mt-20 sm:mb-28 w-[min(100%, 38rem)]">
+			<SectionHeading>Contact Me</SectionHeading>
+			<p className="text-gray-700">
+				Please contact me directly at{' '}
+				<a href="mailto:nikitahsieh03@gmail.com" className="underline">
+					nikitahsieh03@gmail.com
+				</a>{' '}
+				or through this form
+			</p>
+			<form className="mt-10 flex flex-col">
+				<input type="email" className="h-14 rounded-lg borderBlack" />
+				<textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+				<button
+					type="submit"
+					className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+				>
+					Submit{' '}
+					<FaPaperPlane
+						className="text-sm opacity-70 transition-all 
+                    group-hover:translate-x-1 group-hover:-translate-y-1"
+					/>
+				</button>
+			</form>
+		</section>
+	)
+}
